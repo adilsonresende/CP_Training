babbb81d361a89a4b27d9db15fbf84bb42e40625
adilsonresende <adilsonresendetp@gmail.com>
2023-01-27 17:09:38 -0300

    Adde ManipulateBits

---
+++ CP_Training.Business/ManiputateBits.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace CP_Training.Business
+{
+    public class ManiputateBits
+    {
+        public long FlippingBits(long n)
+        {
+            string bitString = Convert.ToString(n, 2).PadLeft(32, '0');
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for(int i = 0; i < bitString.Length; i++)
+            {
+               if(bitString[i] == '0')
+                {
+                    stringBuilder.Append("1");
+                }
+                else
+                {
+                    stringBuilder.Append("0");
+                }
+            }
+
+            ulong result = Convert.ToUInt64(stringBuilder.ToString(), 2);
+            
+            return (long)result;
+        }
+    }
+}
---
+++ CP_Training.UnitTests/ManiputateBitsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using CP_Training.Business;
+
+namespace CP_Training.UnitTests
+{
+    public class ManiputateBitsTests
+    {
+        private ManiputateBits _maniputateBits;
+        [SetUp]
+        public void Setup()
+        {
+            _maniputateBits = new ManiputateBits();
+        }
+
+        [TestCase(1, 4294967294)]
+        [TestCase(0, 4294967295)]
+        [TestCase(2147483647, 2147483648)]
+        public void FlippingBits_WheCalled_ReturnsFlippedNumber(long number, long expectedResult)
+        {
+            long result = _maniputateBits.FlippingBits(number);
+
+            Assert.That(result == expectedResult);
+        }
+    }
+}
