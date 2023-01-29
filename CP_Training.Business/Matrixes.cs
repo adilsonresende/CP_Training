baf13458640a774444835d8a217981654feb3cc5
adilsonresende <adilsonresendetp@gmail.com>
2023-01-29 18:21:37 -0300

    Added matrixes tests

---
+++ CP_Training.Business/Matrixes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP_Training.Business
+{
+    public class Matrixes
+    {
+        public int DiagonalDifference(List<List<int>> arr)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CP_Training.UnitTests/MatrixesTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CP_Training.Business;
+
+namespace CP_Training.UnitTests
+{
+    public class MatrixesTests
+    {
+        private Matrixes _matrixes;
+        [SetUp]
+        public void Setup()
+        {
+            _matrixes = new Matrixes();
+        }
+
+        [Test]
+        public void DiagonalDifference_WhenCalled_ReturnDiagonalDifferenceFromLeftAndRigthDiagonals()
+        {
+            List<List<int>> imput = new List<List<int>>()
+            {
+                new List<int>{ 11, 2, 4 },
+                new List<int>{4, 5, 6},
+                new List<int>{ 10, 8, - 12 }
+            };
+
+            int expecteResult = 15;
+
+            int result = _matrixes.DiagonalDifference(imput);
+
+            Assert.That(result == expecteResult);
+        }
+    }
+}
