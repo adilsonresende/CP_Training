fbf0cdc6ec6e78e0610d6476275e6659f29c89b3
adilsonresende <adilsonresendetp@gmail.com>
2023-02-06 23:57:28 -0300

    Changed project structure

---
+++ CP_Training.Business/Week2/CountingSort1Business.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP_Training.Business.Week2
+{
+    public class CountingSort1Business
+    {
+        public List<int> CountingSort(List<int> arr)
+        {
+            int[] result = new int[100];
+
+            foreach(int currentNumber in arr)
+            {
+                result[currentNumber]++;
+            }
+
+            return result.ToList();
+        }
+    }
+}
---
+++ CP_Training.Business/Week2/Matrixes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CP_Training.Business
+{
+    public class Matrixes
+    {
+        public int DiagonalDifference(List<List<int>> arr)
+        {
+            int numberOfColuns = arr[0].Count;
+            int totalDiagonal = 0;
+            int totalReversedDiagonal = 0;
+            
+            int currentColumn = 0;
+
+            for(int currentRow = 0; currentRow < numberOfColuns; currentRow++)
+            {
+                totalDiagonal += (arr[currentRow][currentColumn]);
+                currentColumn++;
+            }
+
+            currentColumn = numberOfColuns - 1;
+            for (int currentRow = 0; currentRow < numberOfColuns; currentRow++)
+            {
+                totalReversedDiagonal +=(arr[currentRow][currentColumn]);
+                currentColumn--;
+            }
+
+            return  Math.Abs(totalDiagonal - totalReversedDiagonal);
+        }
+    }
+}
